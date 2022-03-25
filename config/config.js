bfe7f744c20c51d05c30eeef65020eee5455a33d
xJacky <nubby0208@outlook.com>
2022-03-25 10:06:13 +0900

    make it better

--- config/config.js
+++ config/config.js
@@ -1,5 +1,5 @@
 const config = {
-    app_name: 'Cool Rarity',
+    app_name: 'Hederarity',
     app_description: 'Cool Rarity is an open source package for easy rarity score calculation with ERC721 NFT metadata collection.',
     collection_file_name: 'collection.json',
     collection_contract_address: '0x5537d90a4a2dc9d9b37bab49b490cf67d4c54e91',
